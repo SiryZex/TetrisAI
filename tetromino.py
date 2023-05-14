84403cc60a5f6d48b9de9cc95095c0cc76f3bed7
SiryZex <219120756@student.uj.ac.za>
2023-05-14 22:34:02 +0200

    Initial Commit: Created Tetris game

---
+++ tetromino.py
@@ -0,0 +1,48 @@
+from settings import *
+import random
+from block import Block
+
+class Tetromino:
+    def __init__(self, tetris, current=True):
+        self.tetris = tetris
+        self.shape = random.choice(list(TETROMINOES.keys()))
+        self.image = random.choice(tetris.app.images)
+        self.blocks = [Block(self, pos) for pos in TETROMINOES[self.shape]]
+        self.landing = False
+        self.current = current
+
+    def rotate(self):
+        pivot_pos = self.blocks[0].pos
+        new_block_positions = [block.rotate(pivot_pos) for block in self.blocks]
+
+        if not self.is_collide(new_block_positions):
+            for i, block in enumerate(self.blocks):
+                block.pos = new_block_positions[i]
+
+    def is_collide(self, block_positions):
+        return any(map(Block.is_collide, self.blocks, block_positions))
+
+    def move(self, direction):
+        move_direction = MOVE_DIRECTIONS[direction]
+        new_block_positions = [block.pos + move_direction for block in self.blocks]
+        is_collide = self.is_collide(new_block_positions)
+
+        if not is_collide:
+            for block in self.blocks:
+                block.pos += move_direction
+        elif direction == 'down':
+            self.landing = True
+
+    def update(self):
+        self.move(direction='down')
+
+
+
+
+
+
+
+
+
+
+
